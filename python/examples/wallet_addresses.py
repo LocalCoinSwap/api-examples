016f836e96b6e7fae9b59cd24d3178b871020be1
codeforcer <nathancworsley@gmail.com>
2020-08-11 14:39:52 +0100

    kusama trade file

--- python/examples/wallet_addresses.py
+++ python/examples/wallet_addresses.py
@@ -9,17 +9,17 @@ exchange_password = os.getenv("ACCOUNT_PASSWORD")
 s = Session()
 s.headers.update({"Authorization": f"Token {api_token}"})
 
-# Kusama addresses from exchange
+# Kusama address from exchange
 url = "https://localcoinswap.com/api/v2/wallets/info/KSM/"
 first_kusama_address = s.get(url).json()['wallets'][0]['address']
 print("Kusama", first_kusama_address)
 
-# Get Bitcoin addresses
+# Bitcoin address
 url = "https://localcoinswap.com/api/v2/wallets/info/BTC/"
 first_bitcoin_address = s.get(url).json()['nc_wallets'][0]['address']
 print("Bitcoin", first_bitcoin_address)
 
-# Get Ethereum addresses
+# Ethereum address
 url = "https://localcoinswap.com/api/v2/wallets/info/ETH/"
 first_ethereum_address = s.get(url).json()['wallets'][0]['address']
 print("Ethereum", first_ethereum_address)
